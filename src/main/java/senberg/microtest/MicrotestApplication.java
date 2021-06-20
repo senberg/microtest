4d4267c2e9e2142e6770df3bf755976961b06be1
Staffan Enberg <senberg@live.se>
2021-06-20 22:44:26 +0200

    initial commit

---
+++ src/main/java/senberg/microtest/MicrotestApplication.java
@@ -0,0 +1,27 @@
+package senberg.microtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MicrotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrotestApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings!";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
